f995ceda7ae40b4fe472c02120447daa5612c35a
xlfsummer <xlf-summer@qq.com>
2018-09-13 18:57:23 +0800

    完成基本流程，命令行基本交互

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ let url = require("url");
 let inquirer = require("inquirer");
 let projectFile = require("./lib/projectFile.js");
 let login = require("./lib/login.js");
+let fs = require("fs");
+let path = require("path");
 
-(async function main() {
+let app = module.exports = {
+    start: () => main().catch(e => {
+        debuggerl
+        console.log(e)
+    }),
+    option: {}
+}
+
+async function main() {
     //首页
     let indexUrl = util.resolveUrl("/");
     let $ = await util.getDocument(indexUrl);
@@ -22,27 +32,68 @@ let login = require("./lib/login.js");
     let classifyList = $(".classify ul");
     let links = util.retriveLinks($, indexUrl, classifyList);
 
-    let answer = await inquirer.prompt([
-        {
-            type: "list",
-            name: "project",
-            message: "select a project to clone",
-            choices: links.map(links => ({ name: links.name, value: links.url }))
+
+    let cmdProjectUrl;
+    if (app.option.project) {
+        cmdProjectUrl = links.find(link => {
+            link.name == app.option.project
+        });
+        if (!cmdProjectUrl) {
+            console.log("产品不存在")
         }
+    }
+
+
+    let answer = await inquirer.prompt([
+        ...(cmdProjectUrl
+            ? []
+            : [{
+                type: "list",
+                name: "project",
+                message: "选择一个产品",
+                choices: links.map(links => ({ name: links.name, value: links.url }))
+            }]
+        ),
+        ...(app.option.user
+            ? []
+            : [{
+                type: "input",
+                name: "user",
+                message: "请输入用户名"
+            }]
+        ),
+        {
+            type: "password",
+            name: "password",
+            message: "请输入密码"
+        },
+        ...(app.option.ignorePsd != null
+            ? []
+            : [{
+                type: "confirm",
+                name: "ignorePsd",
+                message: "是否忽略 psd 文件",
+                default: true
+            }]
+        ),
     ]);
 
-    /** 登录 */
-    await login.login(
+    let user = app.option.user || answer.user;
+    let projectUrl = cmdProjectUrl || answer.project;
+    let ignorePsd = app.option.ignorePsd != null ? app.option.ignorePsd : answer.ignorePsd
 
-    );
+    let batPath = path.resolve(process.cwd(), "update.bat");
+    if (!fs.existsSync(batPath)) {
+        fs.writeFileSync(batPath, `"chcp 65001" && "ued -p ${links.find(link => link.url == projectUrl).name} -u ${user} ${ignorePsd ? "" : "--no-ignore-psd"}"`, "utf8");
+    }
+
+    /** 登录 */
+    await login.login(user, answer.password);
 
     // 项目
-    let projectUrl = answer.project;
     let projectFiles = await projectFile.getProjectFileInfo(projectUrl);
 
-    projectFile.downloadAll(projectFiles);
+    projectFile.downloadAll(projectFiles, ignorePsd);
 
     // classifyList
-})().catch(e => {
-    console.error(e);
-})
+}
--- bin/ued.js
+++ bin/ued.js
@@ -1,3 +1,21 @@
 #!/usr/bin/env node
 
-console.log("hello world")
+let app = require("../app.js");
+
+let program = require("commander");
+
+program
+    .version("0.1.0", "-v --version")
+    .option("-p, --project <project>", "指定要下载的远程项目名称")
+    .option("-u, --user <user>", "指定要登录远程服务器的用户名")
+    .option("--no-ignore-psd", "下载时默认排除PSD文件，使用此选项则取消排除PSD")
+    .parse(process.argv)
+
+let options = {
+    project: program.project,
+    user: program.user,
+    ignorePsd: program.ignorePsd
+};
+
+app.options = options;
+app.start();
