5af96fadc39763ad1bdde1adef73198cfe946dee
xlfsummer <xlf-summer@qq.com>
2018-09-14 11:39:12 +0800

    remove unuse files and packages

--- chalk.js
+++
@@ -1,6 +0,0 @@
-let chalk = require("chalk")
-console.log(chalk.yellow("304"));
-process.stdout.write(chalk.yellow("304"))
-process.stdout.write(chalk.green("200"))
-process.stdin.resume();
-process.stdin.on("data", () => { process.exit(0) })
